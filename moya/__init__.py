3e8eed0bf59aacf0187ab7fe00ae2e854450cfb6
Will McGugan <willmcgugan@gmail.com>
2015-03-10 21:50:42 +0000

    version bump

--- moya/__init__.py
+++ moya/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 __author__ = "Will McGugan <admin@moyaproject.com>"
-__version__ = "0.5.9"
+__version__ = "0.5.10"
 # *** Don't forget to update version in setup.py ***
 
 __all__ = ['pilot', 'expose']
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-VERSION = "0.5.9"
+VERSION = "0.5.10"
 # Don't forget to update version in moya/__init__.py
 
 classifiers = [
