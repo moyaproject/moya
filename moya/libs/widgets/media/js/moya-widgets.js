c8969fd328e76e0c9c2f466a302c63ee7b6e4f90
Will McGugan <willmcgugan@gmail.com>
2015-05-10 13:58:59 +0100

    widget media

---
+++ moya/libs/widgets/media/js/moya-widgets.js
@@ -0,0 +1,125 @@
+(function($) {
+
+    $.fn.moyaSelect = function(options) {
+
+        var options = options || {};
+        var UP=38;
+        var DOWN=40;
+        var RETURN=13;
+        var ESCAPE=27;
+        var TAB = 9;
+
+        var $this = $(this);
+        var max_result = $this.data('count') - 1;
+
+        var selection = -1;
+
+        var $input = $this.find('input');
+        var $options = $this.find('.moya-widgets-select')
+
+
+        var height = 0;
+        $options.find('.moya-widgets-option').each(function(i, el){
+            if(i>=10)
+            {
+                return false;
+            }
+            height += $(el).outerHeight();
+        });
+        var gutter = $options.outerHeight() - $options.innerHeight();
+        height += gutter;
+        $options.css('max-height', height + 'px');
+
+
+        $input.focus(function(e){
+            $this.addClass('focused');
+        });
+
+        $input.blur(function(e){
+            $this.removeClass('focused');
+        });
+
+        $this.find('.moya-widgets-option').click(function(e){
+            e.preventDefault();
+            var $option = $(this);
+            $this.find('.moya-widgets-option').removeClass('active');
+            $option.addClass('active');
+            selection = $option.data('index');
+            var value = $option.data('value');
+            $input.val(value);
+            $input.focus();
+        });
+
+        function refresh_selection()
+        {
+            $this.find('.moya-widgets-option').removeClass('active');
+            $this.find('.moya-widgets-option.option-' + selection).addClass('active');
+            var value = $this.find('.moya-widgets-option.active').data('value');
+            $input.val(value);
+
+
+            var $active = $options.find('.moya-widgets-option.active');
+            var row_h = $active.outerHeight();
+            var container_h = $options.height();
+            var scroll = $options.scrollTop();
+            var y = $active.offset().top - $options.offset().top + scroll - 1;
+
+            if (y - scroll + row_h > container_h)
+            {
+                $options.scrollTop(y - container_h + row_h);
+            }
+            else if (y - scroll < 0)
+            {
+                $options.scrollTop(y);
+            }
+        }
+
+        $input.keydown(function(e)
+        {
+            if (e.which==UP)
+            {
+                if (selection > 0)
+                {
+                    selection -= 1;
+                    refresh_selection();
+                }
+                e.preventDefault();
+            }
+            else if (e.which==DOWN)
+            {
+                if (selection == -1)
+                {
+                    selection = 0;
+                    refresh_selection();
+                }
+                else if (selection < max_result)
+                {
+                    selection += 1;
+                    refresh_selection();
+                }
+                e.preventDefault();
+            }
+            else if (e.which==RETURN)
+            {
+                if(!$results.is(':visible'))
+                {
+                    return;
+                }
+                $results.hide();
+                e.preventDefault();
+                if(selection >= 0)
+                {
+                    var value = $results.find('.selection.active').data('value');
+                    if(value)
+                    {
+                        $input.val(value);
+                        selection = -1;
+                        display_search = null;
+                    }
+                }
+
+            }
+        });
+    }
+
+})(jQuery);
