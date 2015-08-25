99a07b501c0dd5107d000b530d652f445f7d80d2
Will McGugan <willmcgugan@gmail.com>
2015-08-25 17:30:47 +0100

    static server refinements

--- moya/serve.py
+++ moya/serve.py
@@ -1,11 +1,11 @@
 from __future__ import unicode_literals
 from __future__ import print_function
 
-from moya.response import MoyaResponse
-from moya.compat import PY2, py2bytes
-from moya import http
-from moya.tools import datetime_to_epoch, md5_hexdigest
-from moya import logic
+from .response import MoyaResponse
+from .compat import PY2, py2bytes
+from . import http
+from .tools import datetime_to_epoch, md5_hexdigest
+from . import logic
 
 from fs.path import basename
 from fs.errors import FSError
@@ -15,26 +15,34 @@ import mimetypes
 import time
 
 
+from . import __version__
+SERVER_NAME = "Moya/{}.{}".format(*__version__.split('.')[:2])
+
+
 def serve_file(req, fs, path, name=None):
-    """Serve a file"""
+    """Serve a static file"""
     res = MoyaResponse()
     mime_type, encoding = mimetypes.guess_type(basename(path))
     if mime_type is None:
         mime_type = b"application/octet-stream" if PY2 else "application/octet-stream"
 
+    # File does not exist
     if not path or not fs.isfile(path):
         raise logic.EndLogic(http.RespondNotFound())
 
     serve_file = None
+    # Get file info
     try:
         file_size = fs.getsize(path)
         info = fs.getinfokeys(path, 'modified_time')
         serve_file = fs.open(path, 'rb')
     except FSError:
+        # Files system read failed for some reason
         if serve_file is not None:
             serve_file.close()
         raise logic.EndLogic(http.RespondNotFound())
     else:
+        # Make a response
         mtime = info.get('modified_time', None)
         if mtime is None:
             mtime = time.time()
@@ -43,11 +51,12 @@ def serve_file(req, fs, path, name=None):
         res.date = datetime.utcnow()
         res.content_type = py2bytes(mime_type)
         res.last_modified = mtime
-        res.etag = "%i-%i-%s" % (mtime, file_size, md5_hexdigest(path))
-        res.server = "Moya/1.0"
+        res.etag = "{}-{}-{}".format(mtime, file_size, md5_hexdigest(path))
+        res.server = SERVER_NAME
         if name is not None:
             res.content_disposition = 'attachment; filename="{}"'.format(name)
 
+        # Detect 304 not-modified
         status304 = False
         if req.if_none_match and res.etag:
             status304 = res.etag in req.if_none_match
@@ -57,6 +66,11 @@ def serve_file(req, fs, path, name=None):
             res.status = 304
             serve_file.close()
         else:
-            res.body_file = serve_file
+            # Use high performance file wrapper if available
+            if 'wsgi.file_wrapper' in req.environ:
+                res.app_iter = req.environ['wsgi.file_wrapper'](serve_file)
+            else:
+                res.body_file = serve_file
+        # Set content length
         res.content_length = file_size
     raise logic.EndLogic(res)
--- moya/wsgi.py
+++ moya/wsgi.py
@@ -265,8 +265,6 @@ class WSGIApplication(object):
     def get_response(self, request, context):
         """Get a response object"""
         fire = self.archive.fire
-
-
         fire(context, "request.start", app=None, sender=None, data={'request': request})
 
         with pilot.manage_request(request, context):
@@ -332,8 +330,6 @@ class WSGIApplication(object):
         context = Context()
         request = MoyaRequest(environ)
 
-        handle_head = request.method == 'HEAD'
-
         response = self.get_response(request, context)
         taken = time() - start
         clock_taken = clock() - start_clock
@@ -349,8 +345,10 @@ class WSGIApplication(object):
                          response.content_length,
                          taken_ms)
 
-        if not handle_head:
-            yield response.body
+        if request.method == 'HEAD':
+            return []
+        else:
+            return response.app_iter
 
         self.archive.fire(context,
                           "request.end",
