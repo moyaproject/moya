21f23c8bed64176a60c582da5c2de5360622e1f8
Will McGugan <willmcgugan@gmail.com>
2015-10-31 13:16:01 +0000

    version bump

--- moya/__init__.py
+++ moya/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 __author__ = "Will McGugan <admin@moyaproject.com>"
-__version__ = "0.5.22"
+__version__ = "0.5.23a0"
 # *** Don't forget to update version in setup.py ***
 
 __all__ = ['pilot', 'expose']
