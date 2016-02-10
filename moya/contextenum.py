fcff301033d89cc600e8a1f44c4403099c18a05c
Will McGugan <willmcgugan@gmail.com>
2016-02-10 23:25:57 +0000

    changed nomenclature from label to name in enum

--- moya/contextenum.py
+++ moya/contextenum.py
@@ -8,42 +8,41 @@ from .compat import implements_to_string, string_types, text_type
 class ContextEnumValue(object):
     """A single value in an enumeration"""
 
-    def __init__(self, enum, enum_id, label, description, group=None):
+    def __init__(self, enum, enum_id, name, description, group=None):
         self.enum = enum
         self.id = enum_id
-        self.label = label
+        self.name = name
         self.description = description
         self.group = group
 
     def __repr__(self):
         return "<enumvalue {}.{} ({})>".format(self.enum.name,
-                                               self.label,
+                                               self.name,
                                                self.id)
 
     def __hash__(self):
-        return hash((self.id, self.label))
+        return hash((self.id, self.name))
 
     def __int__(self):
         return self.id
 
     def __str__(self):
-        return self.label
+        return self.name
 
     def __moyadbobject__(self):
         return self.id
 
     def __moyaconsole__(self, console):
-        console("<value '{}.{}' ({}))>".format(self.label,
-                                               self.id,
-                                               self.enum.name,
-                                               self.label), bold=True, fg="magenta").nl()
+        console("<enumvalue '{}.{}' ({}))>".format(self.enum.name,
+                                                   self.name,
+                                                   self.id), bold=True, fg="magenta").nl()
 
     def __eq__(self, other):
         # Other enum values only compare if they are the same type
         if isinstance(other, ContextEnumValue):
             return self.enum == other.enum and self.id == other.id
         if isinstance(other, string_types):
-            return self.label == other
+            return self.name == other
         try:
             return self.id == int(other)
         except ValueError:
@@ -67,20 +66,20 @@ class ContextEnum(object):
         console.text(repr(self), fg="green", bold=True)
         table = []
         for value in sorted(self._values, key=int):
-            table.append([value.label, value.id, value.description or ''])
-        console.table(table, header_row=("label", "id", "description"))
+            table.append([value.name, value.id, value.description or ''])
+        console.table(table, header_row=("name", "id", "description"))
 
     def __eq__(self, other):
         if isinstance(other, ContextEnum):
             return self.name == other.name
         return False
 
-    def add_value(self, label, enum_id=None, description=None, group=None):
+    def add_value(self, name, enum_id=None, description=None, group=None):
         if enum_id is None:
             enum_id = self._last_id + 1
-        value = ContextEnumValue(self, enum_id, label, description, group=group)
+        value = ContextEnumValue(self, enum_id, name, description, group=group)
         self._values.append(value)
-        self._label_map[value.label] = value
+        self._label_map[value.name] = value
         self._id_map[value.id] = value
         self._last_id = enum_id
         return value
@@ -113,11 +112,11 @@ class ContextEnum(object):
 
     @property
     def choices(self):
-        return [(e.label, e.description or e.label) for e in self]
+        return [(e.name, e.description or e.name) for e in self]
 
     @property
     def intchoices(self):
-        return [(e.id, e.description or e.label) for e in self]
+        return [(e.id, e.description or e.name) for e in self]
 
     def keys(self):
         return [int(value) for value in self._values] + [text_type(value) for value in self._values]
